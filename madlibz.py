d22ae385aeb49ebc4c21017240af4841263738ac
beardedchinchilla <colton.gilpin1@gmail.com>
2015-01-14 13:17:39 -0600

    Update madlibz.py

--- madlibz.py
+++ madlibz.py
@@ -1,6 +1,56 @@
 print "this is a jedi training diary, by yoda. lets get this over with."
 
 
-print "adjective: %r" %
-raw_input()
+a = print "adjective"
+    raw_input()
 
+b = print "person in room"
+    raw_input()
+
+c = print "noun"
+    raw_input()
+    
+d = print "adjective"
+    raw_input()
+  
+e = print "adjective"
+    raw_input()
+    
+f = print "same person in room"
+    raw_input()
+    
+g = print "noun"
+    raw_input()
+
+h = print "noun"
+    raw_input()
+    
+i = print "noun"
+    raw_input()
+
+j = print "verb"
+    raw_input()
+    
+k = print "type of liquid"
+    raw_input()
+    
+l = print "noun"
+    raw_input()
+    
+m = print "last name"
+
+
+print """
+learning I am if this new Padawan is ready to be a/an %r
+jedi. %r has shown talent with the Force, but 
+%r, he/she seems to be lacking. a jedi worrior
+should not forget where they leave their %r
+lightsaber, no matter how %r they were training
+the night before. %r was not able to use the
+%r to lift an X-wing %r out of the
+%r yesterday, even when i said to %r
+harder. and being dropped into the swampy %r
+i do not like, when a padawan forgets to fasten the buckles on his/her
+%r before doing backflips. the force tells me there
+is another %r, but take my chances with that one,
+i think i will not!
